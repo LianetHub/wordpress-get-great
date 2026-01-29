5ac6eb8ce1ca7c8e473db4d8e085003fd32d2d45
Lianet <Jack-Tolmak@ya.ru>
2026-01-29 13:09:01 +0300

    add template files

--- front-page.php
+++ front-page.php
@@ -9,6 +9,64 @@
 
 <?php require_once(TEMPLATE_PATH . '_promo.php'); ?>
 <?php require_once(TEMPLATE_PATH . '_clients-marquee.php'); ?>
+<section class="cases">
+    <div class="container">
+        <div class="cases__header">
+            <div class="cases__hint hint">портфолио</div>
+            <h2 class="cases__title title">Наши кейсы</h2>
+            <div class="cases__subtitle subtitle">Мы знаем, что такое брендбук, стиль, первые лица, «надо вчера»</div>
+        </div>
+        <ul class="cases__list">
+            <li class="cases__item">
+                <a href="" class="case-card">
+                    <span class="case-card__image">
+                        <img src="https://developer.gektor-studio.com/backend4/get-great/wp-content/uploads/2026/01/brending-wrapper.jpg" alt="Фото кейса">
+                    </span>
+                    <span class="case-card__content">
+                        <span class="case-card__labels">
+                            <span class="case-card__label">Выставочные стенды</span>
+                        </span>
+                        <span class="case-card__footer">
+                            <span class="case-card__name">
+                                Выставочный стенд на «Здравоохранении - 2022»
+                            </span>
+                            <span class="case-card__client">
+                                <span class="case-card__client-caption">Клиент:</span>
+                                <span class="case-card__client-details">
+                                    Компания Medical System&Technology (ООО «МСТ»)
+                                </span>
+                            </span>
+                        </span>
+                    </span>
+                </a>
+            </li>
+        </ul>
+        <a href="" class="cases__more btn btn-primary">Смотреть все 96 кейсов</a>
+    </div>
+</section>
+<section class="about">
+    <div class="container">
+        <div class="about__body">
+            <div class="about__image">
+                <img src="https://developer.gektor-studio.com/backend4/get-great/wp-content/uploads/2026/01/brending-wrapper.jpg" class="cover-image" alt="Фото кейса">
+            </div>
+            <div class="about__content">
+                <div class="about__hint hint">О НАС</div>
+                <h2 class="about__title title">Организация мероприятий и&nbsp;застройка <span class="color-accent">выставочных стендов</span></h2>
+                <div class="about__tabs">
+                    <button class="about__tab btn btn-secondary active">О компании</button>
+                    <button class="about__tab btn btn-secondary">Наши принципы</button>
+                    <button class="about__tab btn btn-secondary">Аккредитация</button>
+                </div>
+                <div class="about__text typography-block">
+                    <p>Наша компания много лет работает в выставочной индустрии и неустанно следим за последними трендами и новинками отрасли. Всё самое лучшее, начиная от технологий и заканчивая дизайном, мы постепенно внедряем в свою деятельность.</p>
+                    <p>В нашем портфолио находятся десятки удачных мероприятий, которые произвели на гостей выставки неизгладимое впечатление. Мы осуществляем оформление интерьеров торговых площадей, уличных павильонов, кафе, кофеен и ресторанов. На каждом этапе проводится тщательная проверка, которая позволяет нам моментально нейтрализовать все нюансы в процессе работы.</p>
+                </div>
+                <a href="" data-fancybox class="about__btn btn btn-primary">Обсудить проект</a>
+            </div>
+        </div>
+    </div>
+</section>
 <?php
 while (have_posts()) :
     the_post();
