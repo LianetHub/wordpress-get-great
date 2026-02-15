884b2efe7edf8e95abd1f6ada9473d58e64c5a87
Lianet <Jack-Tolmak@ya.ru>
2026-02-15 17:11:24 +0300

    remove initial widgets in console

--- includes/admin-custom.php
+++ includes/admin-custom.php
@@ -4,33 +4,31 @@ class Wptuts_Simple_Admin
 {
 	function __construct()
 	{
-		// Hook onto the action 'admin_menu' for our function to remove menu items
 		add_action('admin_menu', array($this, 'remove_menus'));
-		// Hook Dashboard Widgets
 		add_action('wp_dashboard_setup', array($this, 'remove_dashboard_widget'));
-		// Hook onto the action 'admin_bar'
 		add_action('wp_before_admin_bar_render', array($this, 'my_admin_bar_render'));
-		// CUSTOM ADMIN LOGIN HEADER LOGO
 		add_action('login_head',  array($this, 'my_custom_login_logo'));
-		// CUSTOM ADMIN LOGIN LOGO LINK		
 		add_filter('login_headerurl', array($this, 'change_wp_login_url'));
-		// CUSTOM ADMIN LOGIN LOGO & ALT TEXT
 		add_filter('login_headertext', array($this, 'change_wp_login_title'));
-		// Custom css: ACF menu
 		add_action('admin_head', array($this, 'custom_css'));
 	}
 
-	// This function removes each menu item
 	function remove_menus()
 	{
 		global $submenu;
-		unset($submenu['themes.php'][6]); // Customize
+		unset($submenu['themes.php'][6]);
 		remove_menu_page('edit-comments.php');
 	}
 
 	function remove_dashboard_widget()
 	{
 		remove_meta_box('dashboard_primary', 'dashboard', 'side');
+		remove_meta_box('dashboard_quick_press', 'dashboard', 'side');
+		remove_meta_box('dashboard_right_now', 'dashboard', 'normal');
+		remove_meta_box('dashboard_site_health', 'dashboard', 'normal');
+		remove_meta_box('dashboard_activity', 'dashboard', 'normal');
+		remove_meta_box('wpseo-dashboard-overview', 'dashboard', 'normal');
+		remove_meta_box('wpseo-wincher-dashboard-overview', 'dashboard', 'normal');
 		remove_action('welcome_panel', 'wp_welcome_panel');
 	}
 
@@ -45,8 +43,6 @@ class Wptuts_Simple_Admin
 	{
 		$css = "<style>";
 		$css .= "#toplevel_page_theme-general-settings .wp-menu-name,#toplevel_page_theme-general-settings2 .wp-menu-name{ background: #0022aa; } #toplevel_page_theme-general-settings:hover .wp-menu-name{ background: #111; }";
-		//$css .= "#adminmenu { transform: translateZ(0); }"; // fix bag Google Chrome
-		//$css .= ".taxonomy-service_category #wp-description-wrap { display: none; }"; // fix bag Google Chrome
 		$css .= "</style>";
 		echo $css;
 	}
@@ -54,15 +50,15 @@ class Wptuts_Simple_Admin
 	function my_custom_login_logo()
 	{
 		echo '
-				<style type="text/css">
-				    .login h1 a {
-				        background: url(' . get_bloginfo('template_directory') . '/assets/img/logo.svg) rgba(24, 29, 39, 0.86) center no-repeat !important;
-				        background-size: 67% !important;
-				        width: 312px!important;
-				        height: 110px!important;
-				    }
-				</style>
-		';
+                <style type="text/css">
+                    .login h1 a {
+                        background: url(' . get_bloginfo('template_directory') . '/assets/img/logo.svg) rgba(24, 29, 39, 0.86) center no-repeat !important;
+                        background-size: 67% !important;
+                        width: 312px!important;
+                        height: 110px!important;
+                    }
+                </style>
+        ';
 	}
 
 	function change_wp_login_url()
@@ -76,19 +72,3 @@ class Wptuts_Simple_Admin
 	}
 }
 $wptuts_simple_admin = new Wptuts_Simple_Admin();
-
-remove_action('wp_head', 'wp_generator');
-remove_action('wp_head', 'rsd_link');
-remove_action('wp_head', 'feed_links_extra', 3);
-remove_action('wp_head', 'feed_links', 2);
-remove_action('wp_head', 'wlwmanifest_link');
-remove_action('wp_head', 'dns-prefetch', 11);
-remove_action('wp_head', 'rest_output_link_wp_head');
-remove_action('wp_head', 'wp_oembed_add_discovery_links');
-remove_action('wp_head', 'rest_output_link_header', 11, 0);
-remove_action('wp_head', 'print_emoji_detection_script', 7);
-remove_action('wp_print_styles', 'print_emoji_styles');
-add_filter('emoji_svg_url', '__return_false');
-
-
-add_filter('wpcf7_autop_or_not', '__return_false');
