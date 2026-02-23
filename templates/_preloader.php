15851acf884d5d6eb674122cf841638995d3c5d3
lianet <jack.tolmak@yandex.ru>
2026-02-23 16:24:12 +0300

    add preloader block

--- templates/_preloader.php
+++ templates/_preloader.php
@@ -1,27 +1,35 @@
 <?php
+$show_preloader = get_field('show_preloader', 'option');
 $is_error_404 = is_404();
 ?>
-<?php if (!$is_error_404): ?>
-    <!-- <div class="preloader">
-    <noscript>
-        <style>
-            .preloader {
-                display: none !important;
-            }
 
-            body.preloading {
-                overflow: visible !important;
-            }
-        </style>
-    </noscript>
-    <div class="preloader__inner">
-        <div class="preloader__logo">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo-white.svg" alt="Лого">
+<?php if ($show_preloader && !$is_error_404): ?>
+    <style>
+        body.preloading {
+            overflow: hidden;
+        }
+    </style>
+
+    <div class="preloader">
+        <noscript>
+            <style>
+                .preloader {
+                    display: none !important;
+                }
+
+                body.preloading {
+                    overflow: visible !important;
+                }
+            </style>
+        </noscript>
+        <div class="preloader__inner">
+            <div class="preloader__logo">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.svg" alt="Лого">
+            </div>
+            <span class="preloader__percentage">
+                <span id="percentage">0</span>%
+            </span>
+            <span class="preloader__text">Пожалуйста, подождите, идёт&nbsp;загрузка...</span>
         </div>
-        <span class="preloader__percentage">
-            <span id="percentage">0</span>%
-        </span>
-        <span class="preloader__text">Пожалуйста, подождите, идёт&nbsp;загрузка...</span>
     </div>
-</div> -->
 <?php endif; ?>
