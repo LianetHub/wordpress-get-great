1f8b2679f9a8260238deb4b4a5bbc35ae844709a
Lianet <Jack-Tolmak@ya.ru>
2026-02-27 10:50:13 +0300

    add update other services block

--- templates/_other-services.php
+++ templates/_other-services.php
@@ -15,34 +15,51 @@ if (is_admin() && $source === 'global' && get_current_screen()->base !== 'toplev
 }
 
 $title = get_field('other_services_title', $prefix) ?: 'Другие услуги';
+$is_custom = get_field('other_services_custom_links', $prefix);
 
-$args = [
-    'post_type'      => 'services',
-    'posts_per_page' => 4,
-    'post__not_in'   => [get_the_ID()],
-    'orderby'        => 'rand',
-];
-
-$query = new WP_Query($args);
-
-if ($query->have_posts()) :
 ?>
 
-    <section class="other-services" id="other-services">
-        <div class="container">
-            <h2 class="other-services__title hint" data-animate="bottom">
-                <?php echo esc_html($title); ?>
-            </h2>
-            <?php if ($query->have_posts()) : ?>
-                <div class="other-services__grid" data-animate="bottom">
-                    <?php while ($query->have_posts()) : $query->the_post(); ?>
+<section class="other-services" id="other-services">
+    <div class="container">
+        <h2 class="other-services__title hint" data-animate="bottom">
+            <?php echo esc_html($title); ?>
+        </h2>
+
+        <div class="other-services__grid" data-animate="bottom">
+            <?php
+            if ($is_custom) :
+                $custom_links = get_field('other_services_links_list', $prefix);
+                if ($custom_links) :
+                    foreach ($custom_links as $link_item) :
+                        $link = $link_item['link'];
+                        if ($link) : ?>
+                            <a href="<?php echo esc_url($link['url']); ?>"
+                                class="other-services__item icon-chevron-right"
+                                target="<?php echo esc_attr($link['target'] ?: '_self'); ?>">
+                                <?php echo esc_html($link['title']); ?>
+                            </a>
+                        <?php endif;
+                    endforeach;
+                endif;
+            else :
+                $args = [
+                    'post_type'      => 'services',
+                    'posts_per_page' => 4,
+                    'post__not_in'   => [get_the_ID()],
+                    'orderby'        => 'rand',
+                ];
+                $query = new WP_Query($args);
+
+                if ($query->have_posts()) :
+                    while ($query->have_posts()) : $query->the_post(); ?>
                         <a href="<?php the_permalink(); ?>" class="other-services__item icon-chevron-right">
                             <?php the_title(); ?>
                         </a>
-                    <?php endwhile;
-                    wp_reset_postdata(); ?>
-                </div>
-            <?php endif; ?>
+            <?php endwhile;
+                    wp_reset_postdata();
+                endif;
+            endif;
+            ?>
         </div>
-    </section>
-<?php endif; ?>
+    </div>
+</section>
